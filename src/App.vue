d58aaa553871f0144c349c71bea237caca3e41fd
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-03 13:37:07 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, RouterLink, useRouter } from 'vue-router'
 import { ConfigProvider, Tabbar, TabbarItem } from 'vant'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 const active = ref(0)
+const router = useRouter()
+watch(active, () => {})
 </script>
 
 <template>
   <ConfigProvider theme="light">
     <RouterView />
     <Tabbar v-model="active">
-      <TabbarItem icon="home-o">Tab</TabbarItem>
-      <TabbarItem icon="search">Tab</TabbarItem>
+      <RouterLink to="/"><TabbarItem icon="home-o">Tab</TabbarItem></RouterLink>
+      <RouterLink to="/about"><TabbarItem icon="search">Tab</TabbarItem></RouterLink>
       <TabbarItem icon="friends-o">Tab</TabbarItem>
       <TabbarItem icon="setting-o">Tab</TabbarItem>
     </Tabbar>
