c5ddffee229ce6e1feea033c0f28e805db51a60d
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-03 12:38:29 +0330

    update

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Vue 3 App',
+        short_name: 'VueApp',
+        description: 'My awesome Vue 3 app!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/app-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/app-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
