7c32dd88a28ff323783e1ce5d1ff07460674ad07
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-03 14:00:40 +0330

    Update SearchView.vue

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, adipisci. Cum expedita veniam ipsam officia quidem sint doloribus sapiente voluptatem? Pariatur recusandae non corrupti vero nam architecto culpa tempora adipisci?
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, adipisci. Cum expedita veniam
+    ipsam officia quidem sint doloribus sapiente voluptatem? Pariatur recusandae non corrupti vero
+    nam architecto culpa tempora adipisci?
+    <Button @click="getMedia"> take </Button>
   </div>
 </template>
+<script setup lang="ts">
+import { Button } from 'vant'
+
+async function getMedia(constraints: MediaStreamConstraints | undefined) {
+  let stream = null
+
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints)
+    alert(stream)
+    /* use the stream */
+  } catch (err) {
+    /* handle the error */
+  }
+}
+</script>
