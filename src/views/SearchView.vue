707c0fa69a4e95caaf95d9023a65ee4e44503b9e
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-03 14:03:28 +0330

    Update SearchView.vue

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,23 +1,45 @@
 <template>
   <div>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, adipisci. Cum expedita veniam
-    ipsam officia quidem sint doloribus sapiente voluptatem? Pariatur recusandae non corrupti vero
-    nam architecto culpa tempora adipisci?
-    <Button @click="getMedia"> take </Button>
+    <video ref="video" autoplay></video>
+    <Button @click="takePicture">Take Picture</Button>
+    <canvas ref="canvas" style="display: none"></canvas>
   </div>
 </template>
+
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { Button } from 'vant'
 
-async function getMedia(constraints: MediaStreamConstraints | undefined) {
-  let stream = null
+const video = ref<HTMLVideoElement | null>(null)
+const canvas = ref<HTMLCanvasElement | null>(null)
 
+async function getMedia() {
+  const constraints = { video: true }
   try {
-    stream = await window.navigator.mediaDevices.getUserMedia(constraints)
-    alert(stream)
-    /* use the stream */
+    const stream = await window.navigator.mediaDevices.getUserMedia(constraints)
+    if (video.value) {
+      video.value.srcObject = stream
+    }
   } catch (err) {
-    /* handle the error */
+    console.error('Error accessing media devices.', err)
   }
 }
+
+function takePicture() {
+  if (video.value && canvas.value) {
+    const context = canvas.value.getContext('2d')
+    if (context) {
+      canvas.value.width = video.value.videoWidth
+      canvas.value.height = video.value.videoHeight
+      context.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height)
+      const imageDataURL = canvas.value.toDataURL('image/png')
+      alert(imageDataURL) // The image as a Data URL
+      // You can also do something with imageDataURL, like sending it to a server or displaying it.
+    }
+  }
+}
+
+onMounted(() => {
+  getMedia()
+})
 </script>
