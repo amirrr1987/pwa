0d38a8501d678f8a56dce88e03f92ee0de8cb7ff
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-11 19:40:41 +0330

    update

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,38 @@
 import { RouterView, RouterLink, useRouter } from 'vue-router'
 import PWABadge from './components/PWABadge.vue'
 
-import { ConfigProvider, Tabbar, TabbarItem } from 'vant'
-import { ref, watch } from 'vue'
+import { Card, ConfigProvider, Tabbar, TabbarItem } from 'vant'
+import { onMounted, ref, watch } from 'vue'
+import axios from 'axios'
+import type { Product } from './types/ProductModel'
 const active = ref(0)
 const router = useRouter()
 watch(active, () => {})
+const products = ref<Product[]>([])
+const getData = async () => {
+  try {
+    const { data } = await axios.get('https://fakestoreapi.com/products')
+    products.value = data
+    console.log('🚀 ~ getData ~ data:', data)
+  } catch (error) {
+    console.log('🚀 ~ getData ~ error:', error)
+  }
+}
+onMounted(async () => getData())
 </script>
 
 <template>
   <ConfigProvider theme="light">
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum mollitia assumenda iusto aliquid qui, labore, ea recusandae eius laboriosam dolor, minus necessitatibus animi. Tempore porro tempora doloribus reiciendis facere?
-    </p>
+    <Card
+      v-for="item in products"
+      :key="item.id"
+      num="2"
+      :price="item.price"
+      :title="item.title"
+      :desc="item.description"
+      :thumb="item.image"
+    />
+
     <RouterView />
     <PWABadge />
     <Tabbar v-model="active">
