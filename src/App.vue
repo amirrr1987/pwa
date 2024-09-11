424cfeece6f05698e08001ac27f618e3974b57ca
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-11 19:25:10 +0330

    ss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { RouterView, RouterLink, useRouter } from 'vue-router'
+import PWABadge from './components/PWABadge.vue'
+
 import { ConfigProvider, Tabbar, TabbarItem } from 'vant'
 import { ref, watch } from 'vue'
 const active = ref(0)
@@ -10,6 +12,7 @@ watch(active, () => {})
 <template>
   <ConfigProvider theme="light">
     <RouterView />
+    <PWABadge />
     <Tabbar v-model="active">
       <TabbarItem icon="home-o" to="/"> Home </TabbarItem>
       <TabbarItem icon="search" to="/about"> Tab </TabbarItem>
